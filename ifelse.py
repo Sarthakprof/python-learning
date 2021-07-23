7e72b968423da0249e74b6100d084b1201e14a1e
Sarthak Srivastava <sarthak.prof@gmail.com>
2021-07-23 12:04:37 +0530

    added if else

---
+++ ifelse.py
@@ -0,0 +1,30 @@
+num = int(input("enter the number?"))  
+if num%2 == 0:  
+    print("Number is even")  
+
+
+a = int(input("Enter a? "));  
+b = int(input("Enter b? "));  
+c = int(input("Enter c? "));  
+if a>b and a>c:  
+    print("a is largest");  
+if b>a and b>c:  
+    print("b is largest");  
+if c>a and c>b:  
+    print("c is largest");  
+
+age = int (input("Enter your age? "))  
+if age>=18:  
+    print("You are eligible to vote !!");  
+else:  
+    print("Sorry! you have to wait !!");  
+
+number = int(input("Enter the number?"))  
+if number==10:  
+    print("number is equals to 10")  
+elif number==50:  
+    print("number is equal to 50");  
+elif number==100:  
+    print("number is equal to 100");  
+else:  
+    print("number is not equal to 10, 50 or 100");  
